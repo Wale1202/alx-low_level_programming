581cdc0e954d6cdb17ac99d084fdc56a3778b288
Wale1202 <adewalefagbodun@gmail.com>
2023-07-13 19:39:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point for the program
+ *
+ * Return: Show exits status of the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
