1637f8fdf9cae146703d196d4923a2aa5ee93e12
Wale1202 <adewalefagbodun@gmail.com>
2023-07-27 19:45:11 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
  * _strncpy - copy a string
  * @dest: destination string
  * @src: source string
+ * @n: number of bytes to be copied
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
