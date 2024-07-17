be3d0e486d416705f2454c11e2c35b0526febc44
Wale Fagbodun <adewalefagbodun@gmail.com>
2024-07-18 00:27:09 +0100

    Oridnary and Advanced task

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
